55ba1f4a81a8a14a830571d7647daad9bd874435
viral rupani <viralrupani12017@gmail.com>
2022-06-27 12:03:30 +0530

    upgrade.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from walls import Walls
 import time
+
+WALL_COLOR = ['green', 'yellow', 'orange', 'red']
 
 screen = Screen()
 screen.tracer(0)
@@ -14,6 +17,19 @@ screen.listen()
 screen.onkey(paddle.go_left, 'Left')
 screen.onkey(paddle.go_right, 'Right')
 
+wall_list = []
+x_pos = -320
+y_pos = 0
+for row in range(4):
+    for col in range(15):
+        walls = Walls()
+        wall_list.append(walls)
+        walls.color(WALL_COLOR[row])
+        walls.goto(x_pos, y_pos)
+        x_pos += 45
+    y_pos += 20
+    x_pos = -320
+
 game_is_on = True
 while game_is_on:
     screen.update()
@@ -25,7 +41,11 @@ while game_is_on:
         ball.bounce_y()
     elif ball.ycor() < -300:
         ball.respawn()
-    elif ball.distance(paddle) < 20:
+    elif ball.distance(paddle) < 30:
         ball.bounce_y()
 
+    for wall in wall_list:
+        if ball.distance(wall) < 30:
+            ball.bounce_y()
+            wall.reset()
 screen.exitonclick()
